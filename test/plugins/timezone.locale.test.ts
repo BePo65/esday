31a2a8f3cb39ebc89d6b8ba839eb6deaa91522d1
Bernhard Pottler <bpottler@nexgo.de>
2026-01-02 11:04:50 +0100

    test: add more tests for parsing with locale

--- test/plugins/timezone.locale.test.ts
+++ test/plugins/timezone.locale.test.ts
@@ -1,4 +1,5 @@
 import { esday } from 'esday'
+import moment from 'moment-timezone'
 import { describe, expect, it } from 'vitest'
 import localeFr from '~/locales/fr'
 import localeZhCn from '~/locales/zh-cn'
@@ -23,20 +24,23 @@ esday.registerLocale(localeZhCn)
 describe('timezone plugin - with locale', () => {
   it.each([
     {
-      timestamp: '07-2025-18 11:55:00',
+      timestamp: '2025 七月 15日 8:10:21 早上 星期日',
       timezone: 'Asia/Taipei',
-      formatString: 'MM-YYYY-DD HH:mm:ss',
+      formatString: 'YYYY MMMM Do h:mm:ss A',
       locale: 'zh-CN',
     },
     {
-      timestamp: '2025 02:15 01-30',
+      timestamp: '2024 Dec. 24. 8:10:21 AM mercredi',
       timezone: 'Europe/Paris',
-      formatString: 'YYYY HH:mm MM-DD',
+      formatString: 'YYYY MMM Do h:mm:ss A',
       locale: 'fr',
     },
   ])(
     'parse "$timestamp" with locale "$locale" and timezone "$timezone" using format "$formatString"',
     ({ timestamp, timezone, formatString, locale }) => {
+      // set locale to use
+      moment.locale(locale)
+
       expectSameObjectTz((esday) => esday.tz(timestamp, formatString, locale, timezone))
       expect(esday.tz(timestamp, formatString, locale, timezone).isValid()).toBeTruthy()
       expectSameValueTz((esday) => esday.tz(timestamp, formatString, locale, timezone).tz())
@@ -44,8 +48,45 @@ describe('timezone plugin - with locale', () => {
   )
 
   it.todo('parse with format, locale and strict')
+  it.each([
+    {
+      timestamp: '2025 七月 15日 8:10:21 早上',
+      timezone: 'Asia/Taipei',
+      formatString: 'YYYY MMMM Do h:mm:ss A',
+      locale: 'zh-CN',
+    },
+    {
+      timestamp: '2024 Dec. 24. 8:10:21 AM',
+      timezone: 'Europe/Paris',
+      formatString: 'YYYY MMM Do h:mm:ss A',
+      locale: 'fr',
+    },
+  ])(
+    'parse strict "$timestamp" with locale "$locale" and timezone "$timezone" using format "$formatString"',
+    ({ timestamp, timezone, formatString, locale }) => {
+      // set locale to use
+      moment.locale(locale)
 
-  it.todo('parse with format, locale and strict with npn-matching format')
+      expectSameObjectTz((esday) => esday.tz(timestamp, formatString, locale, true, timezone))
+      expect(esday.tz(timestamp, formatString, locale, true, timezone).isValid()).toBeTruthy()
+      expectSameValueTz((esday) => esday.tz(timestamp, formatString, locale, true, timezone).tz())
+    },
+  )
+
+  it('parse with format, locale and strict with non-matching format', () => {
+    // HACK const timestamp = '2024 déc. 24. 8:10:21 AM'
+    // const timestamp = '2024 déc. 24 8:10:21 AM'
+    const timestamp = '2024 déc. 1er 8:10:21 AM'
+    const timezone = 'Europe/Paris'
+    // HACK const formatString = 'YYYY MMM Do h:mm:ss A'
+    const formatString = 'YYYY MMM Do h:mm:ss A'
+    const locale = 'fr'
+    const d = esday.tz(timestamp, formatString, locale, true, timezone)
+    const m = moment.tz(timestamp, formatString, locale, true, timezone)
+
+    expect(d.isValid()).toBeFalsy()
+    expect(m.isValid()).toBeFalsy()
+  })
 
   it('convert keeps the locale', () => {
     const timestamp = '2025-07-13 17:05'
