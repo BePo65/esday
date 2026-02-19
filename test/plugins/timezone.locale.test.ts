a988717713611cfbbd7aa58117479a4c0d0f8ce7
Bernhard Pottler <bpottler@nexgo.de>
2026-02-19 15:04:55 +0100

    test: plugin timezone

---
+++ test/plugins/timezone.locale.test.ts
@@ -0,0 +1,215 @@
+import { esday, type UnitTypeGetSet } from 'esday'
+// @ts-expect-error only type definition is missing, but we do not need it
+import fr from 'moment/dist/locale/fr'
+// @ts-expect-error only type definition is missing, but we do not need it
+import zhCN from 'moment/dist/locale/zh-cn'
+import moment from 'moment-timezone'
+import { describe, expect, it } from 'vitest'
+import localeEn from '~/locales/en'
+import localeFr from '~/locales/fr'
+import localeZhCn from '~/locales/zh-cn'
+import advancedParsePlugin from '~/plugins/advancedParse'
+import localePlugin from '~/plugins/locale'
+import localizedParsePlugin from '~/plugins/localizedParse'
+import timezonePLugin from '~/plugins/timezone'
+import utcPlugin from '~/plugins/utc'
+import weekPlugin from '~/plugins/week'
+import { expectSameObjectTz, expectSameValueTz } from './timezone-test-util'
+
+// we do have to add locales to moment-timezone for browser testing
+// see https://github.com/moment/moment-timezone/issues/647#issuecomment-3232648209
+moment.updateLocale('fr', fr._config)
+moment.updateLocale('zh-cn', zhCN._config)
+
+esday
+  .extend(utcPlugin)
+  .extend(advancedParsePlugin)
+  .extend(localizedParsePlugin)
+  .extend(weekPlugin)
+  .extend(timezonePLugin)
+  .extend(localePlugin)
+
+esday.registerLocale(localeEn)
+esday.registerLocale(localeFr)
+esday.registerLocale(localeZhCn)
+
+describe('timezone plugin - with locale', () => {
+  it.each([
+    {
+      timestamp: '2025 七月 15日 8:10:21 早上 星期日',
+      timezone: 'Asia/Taipei',
+      formatString: 'YYYY MMMM Do h:mm:ss A',
+      locale: 'zh-CN',
+    },
+    {
+      timestamp: '2024 déc. 24. 8:10:21 AM mercredi',
+      timezone: 'Europe/Paris',
+      formatString: 'YYYY MMM Do h:mm:ss A',
+      locale: 'fr',
+    },
+  ])('parse "$timestamp" with locale "$locale" and timezone "$timezone" using format "$formatString"', ({
+    timestamp,
+    timezone,
+    formatString,
+    locale,
+  }) => {
+    // set locale to use
+    moment.locale(locale)
+
+    expectSameObjectTz((esday) => esday.tz(timestamp, formatString, locale, timezone))
+    expect(esday.tz(timestamp, formatString, locale, timezone).isValid()).toBeTruthy()
+    expectSameValueTz((esday) => esday.tz(timestamp, formatString, locale, timezone).tz())
+    expectSameValueTz((esday) =>
+      esday.tz(timestamp, formatString, locale, timezone).locale().toLowerCase(),
+    )
+  })
+
+  it.each([
+    {
+      timestamp: '2025 七月 15日 8:10:21 早上',
+      timezone: 'Asia/Taipei',
+      formatString: 'YYYY MMMM Do h:mm:ss A',
+      locale: 'zh-CN',
+    },
+    {
+      timestamp: '2024 déc. 24 8:10:21 AM',
+      timezone: 'Europe/Paris',
+      formatString: 'YYYY MMM Do h:mm:ss A',
+      locale: 'fr',
+    },
+    {
+      timestamp: '2024 déc. 1er 8:10:21 AM',
+      timezone: 'Europe/Paris',
+      formatString: 'YYYY MMM Do h:mm:ss A',
+      locale: 'fr',
+    },
+  ])('parse strict "$timestamp" with locale "$locale" and timezone "$timezone" using format "$formatString"', ({
+    timestamp,
+    timezone,
+    formatString,
+    locale,
+  }) => {
+    // set locale to use
+    moment.locale(locale)
+
+    expectSameObjectTz((esday) => esday.tz(timestamp, formatString, locale, true, timezone))
+    expect(esday.tz(timestamp, formatString, locale, true, timezone).isValid()).toBeTruthy()
+    expectSameValueTz((esday) => esday.tz(timestamp, formatString, locale, true, timezone).tz())
+    expectSameValueTz((esday) =>
+      esday.tz(timestamp, formatString, locale, true, timezone).locale().toLowerCase(),
+    )
+  })
+
+  it('parse with format, locale and strict with non-matching format', () => {
+    const timestamp = '2024 déc. 24. 8:10:21 AM'
+    const timezone = 'Europe/Paris'
+    const formatString = 'YYYY MMM Do h:mm:ss A'
+    const locale = 'fr'
+    const d = esday.tz(timestamp, formatString, locale, true, timezone)
+    const m = moment.tz(timestamp, formatString, locale, true, timezone)
+
+    expect(d.isValid()).toBeFalsy()
+    expect(m.isValid()).toBeFalsy()
+    expect(d.locale()).toBe(locale)
+    expect(m.locale()).toBe(locale)
+  })
+
+  it('convert keeps the locale', () => {
+    const timestamp = '2025-07-13 17:05'
+    const timezone = 'Europe/Paris'
+    const locale = 'fr'
+    const base = esday(timestamp).locale(locale)
+    const converted = base.tz(timezone)
+
+    expect(base.locale()).toBe(locale)
+    expect(converted.locale()).toBe(locale)
+  })
+
+  it.each(['d', 'day', 'days'])('get with unit "%s" for locale en', (unit) => {
+    const locale = 'en'
+    // set locale to use
+    esday.locale(locale)
+    moment.locale(locale)
+
+    const timestamp = '2025-07-23 21:25:36.345'
+    const timezone = 'America/New_York'
+
+    expectSameValueTz((esday) => esday.tz(timestamp, timezone).get(unit as UnitTypeGetSet))
+  })
+
+  it.each(['d', 'day', 'days'])('get with unit "%s" for locale fr', (unit) => {
+    const locale = 'fr'
+    // set locale to use
+    esday.locale(locale)
+    moment.locale(locale)
+
+    const timestamp = '2025-07-23 14:25:36.345'
+    const timezone = 'Europe/Paris'
+
+    expectSameValueTz((esday) => esday.tz(timestamp, timezone).get(unit as UnitTypeGetSet))
+  })
+
+  it.each([
+    { timestamp: '2025-02-09 11:15:09', timezone: 'America/Toronto', expected: 'EST' },
+    { timestamp: '2025-07-09 11:15:09', timezone: 'America/Toronto', expected: 'EDT' },
+    { timestamp: '2025-02-09 11:15:09', timezone: 'Europe/London', expected: 'GMT' },
+    { timestamp: '2025-07-09 11:15:09', timezone: 'Europe/London', expected: 'GMT+1' },
+    { timestamp: '2025-02-09 11:15:09', timezone: 'Europe/Paris', expected: 'GMT+1' },
+    { timestamp: '2025-07-09 11:15:09', timezone: 'Europe/Paris', expected: 'GMT+2' },
+    { timestamp: '2025-02-09 11:15:09', timezone: 'Australia/Canberra', expected: 'GMT+11' },
+    { timestamp: '2025-07-09 11:15:09', timezone: 'Australia/Canberra', expected: 'GMT+10' },
+  ])('format "$timestamp" for "$timezone" for default locale en', ({
+    timestamp,
+    timezone,
+    expected,
+  }) => {
+    // set global locale to use; only 'en' has names for timezone independent of locale of runtime
+    const locale = 'en'
+    esday.locale(locale)
+    moment.locale(locale)
+
+    expect(esday.tz(timestamp, timezone).format('z')).toBe(expected)
+  })
+
+  it.each([
+    { timestamp: '2025-02-09 11:15:09', timezone: 'America/Toronto', expected: 'EST' },
+    { timestamp: '2025-07-09 11:15:09', timezone: 'America/Toronto', expected: 'EDT' },
+    { timestamp: '2025-02-09 11:15:09', timezone: 'Europe/London', expected: 'GMT' },
+    { timestamp: '2025-07-09 11:15:09', timezone: 'Europe/London', expected: 'GMT+1' },
+    { timestamp: '2025-02-09 11:15:09', timezone: 'Europe/Paris', expected: 'GMT+1' },
+    { timestamp: '2025-07-09 11:15:09', timezone: 'Europe/Paris', expected: 'GMT+2' },
+    { timestamp: '2025-02-09 11:15:09', timezone: 'Australia/Canberra', expected: 'GMT+11' },
+    { timestamp: '2025-07-09 11:15:09', timezone: 'Australia/Canberra', expected: 'GMT+10' },
+  ])('format "$timestamp" for "$timezone" for locale en', ({ timestamp, timezone, expected }) => {
+    // locale to use; only 'en' has names for timezone independent of locale of runtime
+    const locale = 'en'
+
+    expect(esday(timestamp).locale(locale).tz(timezone).format('z')).toBe(expected)
+  })
+})
+
+describe('timezone plugin - with plugin week', () => {
+  it.each(['w', 'week', 'weeks'])('get with unit "%s" for locale en', (unit) => {
+    const locale = 'en'
+    // set locale to use
+    esday.locale(locale)
+    moment.locale(locale)
+
+    const timestamp = '2025-07-20 14:25:36.345' // calendar week 30
+    const timezone = 'America/New_York'
+
+    expectSameValueTz((esday) => esday.tz(timestamp, timezone).get(unit as UnitTypeGetSet))
+  })
+
+  it.each(['w', 'week', 'weeks'])('get with unit "%s" for locale fr', (unit) => {
+    const locale = 'fr'
+    // set locale to use
+    esday.locale(locale)
+    moment.locale(locale)
+
+    const timestamp = '2025-07-20 14:25:36.345' // calendar week 29
+    const timezone = 'Europe/Paris'
+
+    expectSameValueTz((esday) => esday.tz(timestamp, timezone).get(unit as UnitTypeGetSet))
+  })
+})
